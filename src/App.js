9e0a2768b2eab875c1bda42624311fd1f26d0376
VIJAY KUMAR MISHRA <vijay.mishra@mountblue.tech>
2020-04-17 09:33:03 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './biglogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>Trello Clone App using ReactJS and Trello REST API</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
